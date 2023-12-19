40f03317d89830b580230b3c36a14774724856d6
Stalloyde <stalloyde93@gmail.com>
2023-12-19 15:16:59 +0800

    Implement login - WIP: sanitise & validate inputs

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,3 +1,13 @@
-exports.login = (req, res, next) => {
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+exports.loginGET = (req, res, next) => {
   res.render('login');
 };
+
+// sanitise and validate before authenticate
+// generate error.msg in view template
+exports.loginPOST = passport.authenticate('local', {
+  failureRedirect: '/login',
+  successRedirect: '/members/basic',
+});
--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,10 +1,13 @@
 const express = require('express');
+// const passport = require('passport');
+// const LocalStrategy = require('passport-local').Strategy;
 const dbConnection = require('../config/db');
 const loginController = require('../controller/loginController');
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', loginController.login);
+router.get('/', loginController.loginGET);
+router.post('/', loginController.loginPOST);
 
 module.exports = router;
