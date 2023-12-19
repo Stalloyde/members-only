fb0baeb3f6d8ca7a95afdf1c733c231d48068f13
Stalloyde <stalloyde93@gmail.com>
2023-12-19 12:26:06 +0800

    Preserve username value on signup error

--- controller/signUpController.js
+++ controller/signUpController.js
@@ -14,23 +14,24 @@ exports.signUpPOST = [
   body('confirmPassword').custom((value, { req }) => value === req.body.password).withMessage('The passwords do not match'),
 
   asyncHandler(async (req, res, next) => {
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      if (err) {
+        return next(err);
+      }
+      return hashedPassword;
+    });
+
+    const newUser = new User({
+      username: req.body.username,
+      password: bcrypt.hashedPassword,
+    });
+
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      res.render('signUp', { errors: errors.array() });
+      res.render('signUp', { errors: errors.array(), user: newUser });
     } else {
-      bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-        if (err) {
-          return next(err);
-        }
-
-        const newUser = new User({
-          username: req.body.username,
-          password: hashedPassword,
-        });
-
-        await newUser.save();
-        res.render('login');
-      });
+      await newUser.save();
+      res.render('login');
     }
   }),
 ];
