d295bb46a3eb83d4f184094912f28472cc3c7f33
Stalloyde <stalloyde93@gmail.com>
2023-12-20 23:23:50 +0800

    Implement members and new message routes

--- controller/membersController.js
+++ controller/membersController.js
@@ -3,7 +3,26 @@ const mongoose = require('mongoose');
 const { body, validationResult } = require('express-validator');
 const asyncHandler = require('express-async-handler');
 const User = require('../models/users');
+const Message = require('../models/message');
 
-exports.membersBasicGET = (req, res, next) => {
-  res.render('membersBasic', { loggedIn: true });
+exports.membersGET = async (req, res, next) => {
+  const [currentUser, messages] = await Promise.all([
+    User.findById(req.params.id),
+    Message.find().populate('user'),
+  ]);
+
+  res.render('membersPage', {
+    currentUser, messages, loggedIn: true, isVip: currentUser.isVip, isMod: currentUser.isMod,
+  });
+};
+
+exports.membersNewMessageGET = async (req, res, next) => {
+  const [currentUser, messages] = await Promise.all([
+    User.findById(req.params.id),
+    Message.find().populate('user'),
+  ]);
+
+  res.render('membersNewMessage', {
+    currentUser, messages, loggedIn: true, isVip: currentUser.isVip, isMod: currentUser.isMod,
+  });
 };
--- routes/membersRoute.js
+++ routes/membersRoute.js
@@ -5,6 +5,7 @@ const membersController = require('../controller/membersController');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/:id/basic', membersController.membersBasicGET);
+router.get('/:id', membersController.membersGET);
+router.get('/:id/new-message', membersController.membersNewMessageGET);
 
 module.exports = router;
